75f0760d0fe26aa36f0dcd21b47d8d22e1a63d29
alrafay41 <55292265+alrafay41@users.noreply.github.com>
2024-03-22 04:44:04 +0500

    jwt service added

---
+++ backEnd/services/JWTservice.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const {
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+} = require("../config/index");
+const refreshToken = require("../models/token");
+
+class JWTService {
+  // the main difference between access and refresh token is that access token has a less expiry time as compared to refresh token. the secret key of both of them could be same,
+  // but the best practice is to keep them dfferent
+
+  signAccessToken(payload, time, secret = ACCESS_TOKEN_SECRET) {
+    return jwt.sign(payload, secret, { expiresIn: time });
+  }
+
+  signRefreshToken(payload, time, secret = REFRESH_TOKEN_SECRET) {
+    return jwt.sign(payload, secret, { expiresIn: time });
+  }
+
+  // we make all the methods static so that when we import it and use it in a file, we don't have to make a new object
+  static verifyAccessToken(token) {
+    return jwt.verify(token, ACCESS_TOKEN_SECRET);
+  }
+
+  static verifyRefreshToken(token) {
+    return jwt.verify(token, REFRESH_TOKEN_SECRET);
+  }
+
+  static async storeRefreshToken(token, userId) {
+    try {
+      const newToken = new refreshToken({ token: token, userId: userId });
+      await newToken.save();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = JWTService;
