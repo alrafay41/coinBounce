803e9d5b6bce629d28c4902a6f94ec56b4fd3503
alrafay41 <55292265+alrafay41@users.noreply.github.com>
2024-04-03 20:36:32 +0500

    blogController CRUD completed

--- backEnd/controllers/blogController.js
+++ backEnd/controllers/blogController.js
@@ -1,8 +1,10 @@
 const Joi = require("joi");
 const fs = require("fs");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 const { BACKEND_SERVER_PATH } = require("../config/index");
 const BlogDTO = require("../DTOs/blog");
+const BlogDetailsDTO = require("../DTOs/blogDetails");
 const mongodbIdPattern = /^[0-9a-fA-F]{24}$/;
 
 const blogController = {
@@ -24,7 +26,7 @@ const blogController = {
     const { error } = createBlogSchema.validate(req.body);
 
     if (error) {
-      next(error);
+      return next(error);
     }
 
     const { title, content, photo, author } = req.body;
@@ -44,7 +46,7 @@ const blogController = {
     try {
       fs.writeFileSync(`storage/${imagePath}`, buffer);
     } catch (error) {
-      next(error);
+      return next(error);
     }
 
     // now we save the data to db, and for that we need to import model of blog first
@@ -54,12 +56,12 @@ const blogController = {
         title,
         content,
         author,
-        photoPath: `BACKEND_SERVER_PATH/storage/${imagePath}`,
+        photoPath: `${BACKEND_SERVER_PATH}/storage/${imagePath}`,
       });
 
       await newBlog.save();
     } catch (error) {
-      next(error);
+      return next(error);
     }
 
     // need to import a blogDTO for the response.
@@ -68,10 +70,141 @@ const blogController = {
 
     return res.status(201).json({ blog: blogDto });
   },
-  async getAll(req, res, next) {},
-  async getById(req, res, next) {},
-  async update(req, res, next) {},
-  async delete(req, res, next) {},
+  async getAll(req, res, next) {
+    try {
+      const blogs = await Blog.find({});
+
+      // to mold it according to dto we use for loop
+      const blogsDto = [];
+
+      for (i = 0; i < blogs.length; i++) {
+        const dto = new BlogDTO(blogs[i]);
+        blogsDto.push(dto);
+      }
+      return res.status(200).json({ blogs: blogsDto });
+    } catch (error) {
+      return next(error);
+    }
+  },
+  async getById(req, res, next) {
+    // validate id
+    // send response
+    const getIdSchema = Joi.object({
+      id: Joi.string().regex(mongodbIdPattern).required(),
+    });
+
+    const { error } = getIdSchema.validate(req.params);
+
+    if (error) {
+      return next(error);
+    }
+    const { id } = req.params;
+    let blog;
+    try {
+      blog = await Blog.findOne({ _id: id }).populate("author");
+    } catch (error) {
+      return next(error);
+    }
+
+    const blogDetailsDto = new BlogDetailsDTO(blog);
+
+    return res.status(200).json({ blog: blogDetailsDto });
+  },
+  async update(req, res, next) {
+    // validate
+    // if we are updating photo, then we delete previous photo
+
+    const updateBlogSchema = Joi.object({
+      title: Joi.string(),
+      content: Joi.string(),
+      photo: Joi.string(),
+      author: Joi.string().regex(mongodbIdPattern).required(),
+      blogId: Joi.string().regex(mongodbIdPattern).requuired(),
+    });
+
+    const { error } = updateBlogSchema.validate(req.body);
+
+    if (error) {
+      next(error);
+    }
+
+    const { title, content, photo, author, blogId } = req.body;
+
+    let blog;
+
+    try {
+      blog = await Blog.findOne({ _id: blogId });
+    } catch (error) {
+      return next(error);
+    }
+
+    if (photo) {
+      let previousPhoto = blog.photoPath; // the previousPhoto is found in the blog, as we have used findOne method.
+      previousPhoto = previousPhoto.split("/").at(-1);
+
+      // delete photo
+
+      fs.unlinkSync(`storage/${previousPhoto}`);
+
+      // read as buffer
+
+      const buffer = Buffer.from(
+        photo.replace(/^data:image\/(png|jpg|jpeg);base64,/, ""),
+        "base64"
+      );
+
+      // allot a random name
+
+      const imagePath = `${date.now()}-${author}.png`;
+
+      // save locally.
+      try {
+        fs.writeFileSync(`storage/${imagePath}`, buffer);
+      } catch (error) {
+        return next(error);
+      }
+
+      await Blog.updateOne(
+        { _id: blogId },
+        {
+          title,
+          content,
+          photoPath: `${BACKEND_SERVER_PATH}/storage/${imagePath}`,
+        }
+      );
+    } else {
+      await Blog.updateOne({ _id: blogId }, { title, content });
+    }
+
+    return res.status(200).json({ message: "Blog updated Successfully!" });
+  },
+  async delete(req, res, next) {
+    //validate id
+    //delete blog
+    //delete comments on that blog
+
+    const deleteIdSchema = Joi.object({
+      id: Joi.regex(mongodbIdPattern).required().string(),
+    });
+
+    const { error } = deleteIdSchema.validate(req.params);
+
+    if (error) {
+      return next(error);
+    }
+
+    const { id } = req.params;
+
+    try {
+      await Blog.deleteOne({ _id: id });
+      // to delete comments on that blog, we need to import comments model
+      await Comment.deleteMany({ blog: id });
+    } catch (error) {
+      return next(error);
+    }
+
+    return res.status(200).json({ message: "Blog Deleted Successfully!" });
+  },
 };
 
 module.exports = blogController;
