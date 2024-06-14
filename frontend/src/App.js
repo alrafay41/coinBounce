2e3a25bf16e82514f64f87b95d6bfef2f85ff86c
alrafay41 <55292265+alrafay41@users.noreply.github.com>
2024-06-15 00:45:43 +0500

    project set up

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+  return (
+<>
+<h1>Hello World</h1>
+<Navbar/>
+</>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import styles from '../navbar/Navbar.module.css'
+
+function Navbar() {
+    return (<h2>Navbar</h2>)
+}
+
+export default Navbar;
